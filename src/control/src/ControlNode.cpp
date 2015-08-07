bdd18935cb4d7dd497aca6207c546e8987390f5b
Matt <matthewia94@gmail.com>
2015-08-07 17:15:42 -0500

    Initialize Velocities
    Previously the velocities at startup were undefined.  They have been
    explicitely set to 0 at startup to avoid the robot running away.  It's
    pretty fast if anyone was wondering.

--- src/control/src/ControlNode.cpp
+++ src/control/src/ControlNode.cpp
@@ -14,6 +14,13 @@ void ControlNode::joy_callback(const sensor_msgs::Joy::ConstPtr& msg)
 
 ControlNode::ControlNode()
 {
+	cmd_vel.linear.x = 0;
+	cmd_vel.linear.y = 0;
+	cmd_vel.linear.z = 0;
+	cmd_vel.angular.x = 0;
+	cmd_vel.angular.y = 0;
+	cmd_vel.angular.z = 0;
+
 	joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, 
 	                                         &ControlNode::joy_callback, this);
 
